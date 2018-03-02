f42eed975d599f474b4c9d2d4a14451a5ef083be
woojunchoi <tim.woojune1@gmail.com>
2018-03-02 05:57:33 -0800

    directives are created

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,6 +1,28 @@
-var space = angular.module('space', []);
+var space = angular.module('space', ['ngRoute']);
 
+space.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+    when('/home', {
+        templateUrl:'/index.html'
+    }).
+    when('/about', {
+        templateUrl:'/public/views/about.html'
+    }).
+    when('/techskills', {
+        templateUrl:'/public/views/tech.html'
+    }).
+    when('/works', {
+        templateUrl:'/public/views/works.html'
+    }).
+    when('/contact', {
+        templateUrl:'/public/views/works.html'        
+    }).
+    otherwise({
+        redirectTo:'/home'
+    })
+}])
 
 space.controller('spaceController' ['$scope','$http', function($scope,$http) {
     $scope.hi = 'hi'
 }])
+
